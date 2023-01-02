58fdd6f46102a624d9600c339ddc250b0d92afc3
Yu Wang <yuwan@microsoft.com>
2023-01-02 11:12:15 -0800

    feat: download pre-sharded models

--- bloom-inference-scripts/download_model_for_ds.py
+++ bloom-inference-scripts/download_model_for_ds.py
@@ -1,8 +1,9 @@
 from huggingface_hub import snapshot_download
 snapshot_download(
-    'microsoft/bloom-deepspeed-inference-int8',
+    'microsoft/bloom-deepspeed-inference-fp16',
     local_files_only=False,
     cache_dir=None,
-    ignore_patterns=["*.safetensors"],
+    # ignore_patterns=["*.safetensors"],
+    ignore_patterns=["*.safetensors", "flax_model*", "tf_model*"],
     resume_download=True
 )
